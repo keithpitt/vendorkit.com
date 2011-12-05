2120edab2b81e8f864fb68792ce1fa25774273dc
Keith Pitt <me@keithpitt.com>
2011-12-05 23:26:34 +1100

    Some more forge->kit updates

--- db/migrate/20111108121120_add_downlaods_count_to_version.rb
+++ db/migrate/20111108121120_add_downlaods_count_to_version.rb
@@ -2,9 +2,9 @@ class AddDownlaodsCountToVersion < ActiveRecord::Migration
   def change
     add_column :versions, :downloads_count, :integer, :default => 0
 
-    VendorForge::Version.reset_column_information
-    VendorForge::Version.all.each { |version|
-      VendorForge::Version.update_counters version.id, :downloads_count => version.downloads.count
+    VendorKit::Version.reset_column_information
+    VendorKit::Version.all.each { |version|
+      VendorKit::Version.update_counters version.id, :downloads_count => version.downloads.count
     }
   end
 end
