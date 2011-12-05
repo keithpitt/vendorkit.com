keithpitt/vendorforge.org

Updates to the home page

app/controllers/pages_controller.rb
@@ -1,7 +1,9 @@
 class PagesController < ApplicationController
 
   def index
-    @latest_vendors = VendorForge::Vendor.latest.limit(5)
+    @latest_vendors = VendorForge::Vendor.latest.includes(:user).limit(5)
+    @downloaded_vendors = VendorForge::Vendor.downloaded.includes(:user).limit(5)
+    @updated_vendors = VendorForge::Vendor.updated.includes(:user).limit(5)
   end
 
 end

keithpitt/vendorkit.com

Conflicts:
	app/controllers/pages_controller.rb

app/controllers/pages_controller.rb
@@ -1,7 +1,9 @@
 class PagesController < ApplicationController
 
   def index
-    @latest_vendors = VendorKit::Vendor.latest.limit(5)
+    @latest_vendors = VendorKit::Vendor.latest.includes(:user).limit(5)
+    @downloaded_vendors = VendorKit::Vendor.downloaded.includes(:user).limit(5)
+    @updated_vendors = VendorKit::Vendor.updated.includes(:user).limit(5)
   end
 
 end