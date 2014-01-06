282cee28a2d57ecf1613bdeeff1acb34e5a021af
Keith Pitt <me@keithpitt.com>
2014-01-07 06:37:48 +0800

    Commenting out routes and adding shutdown page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,14 @@
 VendorKit::Application.routes.draw do
+  # devise_for :users, :class_name => "VendorKit::User"
 
-  devise_for :users, :class_name => "VendorKit::User"
+  # root :to => 'pages#index'
 
-  root :to => 'pages#index'
+  # match "documentation", :to => "pages#documentation"
 
-  match "documentation", :to => "pages#documentation"
+  # resources :vendors, :only => [ :index, :new, :create, :show, :destroy ] do
+  #   get "versions/:version" => "vendors#show", :as => :version, :constraints  => { :version => /[0-z\.]+/ }
+  #   get "versions/:version/download" => "vendors#download", :as => :download, :constraints  => { :version => /[0-z\.]+/ }
+  # end
 
-  resources :vendors, :only => [ :index, :new, :create, :show, :destroy ] do
-    get "versions/:version" => "vendors#show", :as => :version, :constraints  => { :version => /[0-z\.]+/ }
-    get "versions/:version/download" => "vendors#download", :as => :download, :constraints  => { :version => /[0-z\.]+/ }
-  end
-
-  match "users/:id/api_key" => "users#api_key", :defaults => { :format => "json" }
-
+  # match "users/:id/api_key" => "users#api_key", :defaults => { :format => "json" }
 end
